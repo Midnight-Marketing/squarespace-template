9fb6721cbf4bc1300289fcd5be838df3d4ce194e
Nathan Paynter <nathan@midnightmarketing.com>
2022-06-01 15:59:26 -0600

    Updated compCount script

--- scripts/modules/compilation.js
+++ scripts/modules/compilation.js
@@ -7,10 +7,16 @@ function compCount() {
   var iC = $('[data-item-count]')
 
   iC.each(function() {
+    // Just for verifying if a leading 0 is needed
     var c = $(this).attr('data-item-count')
+
+    // Don't run if greater than 10
     if (c >= 10) return
 
-    c = '0' + c
-    $(this).text(c)
+    // Get current text to modify
+    var t = $(this).text()
+
+    t = '0' + t
+    $(this).text(t)
   })
 }
